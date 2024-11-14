6bb4e1d654e60d738b66d8b4d079a1d49c6160db
unknown <rakunnaka@gmail.com>
2024-11-14 10:06:50 +0700

    fixed profile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -9,7 +9,7 @@ const Profile = () => {
 
   return (
     <div className="flex items-center justify-evenly h-screen gap-8 flex-wrap ">
-      <div className="flex flex-col items-center text-center">
+      <div className="flex flex-col ">
         <h5 className="mb-3 text-2xl md:text-3xl font-medium text-gray-800 dark:text-white">
           HI! I am
         </h5>
@@ -23,6 +23,7 @@ const Profile = () => {
 
       <div className='flex gap-10'>
         <Button classname="bg-slate-700 text-slate-50 hover:bg-slate-50 hover:text-slate-700">Download Cv</Button>
+
         <a href='#about'>
         <Button>Contant me</Button>
         </a>
@@ -34,8 +35,10 @@ const Profile = () => {
       <img
         alt="image"
         src={pp}
-        className="w-40 h-40 sm:w-60 sm:h-60 lg:w-72 lg:h-72 rounded-3xl shadow-lg"
+        className="hidden md:block w-40 h-40 sm:w-60 sm:h-60 lg:w-72 lg:h-72 rounded-3xl shadow-lg"
       />
+
+    
 
      
 
