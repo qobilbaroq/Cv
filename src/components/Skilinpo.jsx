773335f43e8fceb9d491487893bf50662464bae2
unknown <rakunnaka@gmail.com>
2024-11-14 19:00:18 +0700

    text

--- src/components/Skilinpo.jsx
+++ src/components/Skilinpo.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Skilinpo = () => {
   return (
-    <div className='flex h-screen'>
+    <div className='flex h-screen justify-center items-center'>
         <h1>lagi dalam proses boss!!</h1>
     </div>
   )
